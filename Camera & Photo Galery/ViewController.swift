af8396ba1fe95fbd7345886222c576975fb19ae6
Christiawan Eko S <christiawaneko@gmail.com>
2018-10-26 12:08:01 +0700

    Simple Camera and Galery

--- Camera & Photo Galery/ViewController.swift
+++ Camera & Photo Galery/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnAmbilGambar(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
+        let alert = UIAlertController(title: "Info", message: "Ambil Gambar", preferredStyle: .actionSheet)
+        
+        let btnCamera = UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                picker.sourceType = .camera
+                self.present(picker,animated: true,completion: nil)
+            }else{
+                print("tidak ada kamera")
+            }
+        }
+        
+        let btnGalery = UIAlertAction(title: "Galery", style: .default) { (UIAlertAction) in
+            picker.sourceType = .photoLibrary
+            self.present(picker,animated: true,completion: nil)
+        }
+       
+        alert.addAction(btnCamera)
+        alert.addAction(btnGalery)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let gambar = info[.originalImage]
+        imgView.image = gambar as! UIImage
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
